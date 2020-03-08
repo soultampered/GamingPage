2f41368b1a285efd257ab5d6c6312f6e6750aba7
soultampered <42450113+soultampered@users.noreply.github.com>
2020-03-08 11:42:58 -0400

    changed js file
    renamed js file

---
+++ gwfStream.js
@@ -0,0 +1,8 @@
+	
+
+	// List of Featured streams
+	var stream1 = "https://player.twitch.tv/?channel=method&autoplay=false";
+	var stream2 = "https://player.twitch.tv/?channel=esl_csgo&autoplay=false";
+	var stream3 = "https://player.twitch.tv/?channel=ellesmere_gaming&autoplay=false";
+	var stream4 = "https://player.twitch.tv/?channel=lec&autoplay=false";
+	var stream5 = "https://player.twitch.tv/?channel=riotgamesjp&autoplay=false";
