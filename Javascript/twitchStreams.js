7d301ed56a7a688ae76da6f87e55d86781912002
soultampered <42450113+soultampered@users.noreply.github.com>
2020-03-08 14:45:50 -0400

    JS changes
    Moved js script to separate file.

--- Javascript/twitchStreams.js
+++ Javascript/twitchStreams.js
@@ -8,3 +8,18 @@ var stream2 = "https://player.twitch.tv/?channel=esl_csgo&autoplay=false";
 var stream3 = "https://player.twitch.tv/?channel=ellesmere_gaming&autoplay=false";
 var stream4 = "https://player.twitch.tv/?channel=lec&autoplay=false";
 var stream5 = "https://player.twitch.tv/?channel=riotgamesjp&autoplay=false";
+
+
+
+
+
+window.onload = replaceSrc;
+
+
+//Picks a randomly selected stream from the above list
+var streamList = [stream1, stream2, stream3, stream4, stream5];
+var randomStream = streamList[Math.floor(Math.random() * streamList.length)];
+
+function replaceSrc() {
+	document.getElementById("streamFrame").src = randomStream;
+}
